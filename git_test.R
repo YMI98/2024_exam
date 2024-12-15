31a3b0390b363aac19dad4d891f181e01080ad44
YMI98 <yangming3001@gmail.com>
2024-12-15 15:58:43 +0000

    test

--- git_test.R
+++ git_test.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 # This is an example of inserting large datasets
-psql_manipulate(cred = cred_psql_docker, 
+psql_look(cred = cred_psql_docker, 
                schema_name = "example_schema",
                tab_name =  "example_table",
                df = data)
